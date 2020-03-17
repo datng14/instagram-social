314511235ceb201f32289410959599f2ca1049d1
Datng <nguyendat1411@gmail.com>
2020-03-18 00:02:35 +0700

    update header menu

--- app/components/Header/NavBar.js
+++ app/components/Header/NavBar.js
@@ -2,4 +2,14 @@ import styled from 'styled-components';
 
 export default styled.div`
   text-align: center;
+  .icon {
+    width: 22px;
+    margin-right: 22px;
+    &-avatar {
+      border-radius: 50%;
+      width: 22px;
+      height: 22px;
+      object-fit: cover;
+    }
+  }
 `;
--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 import A from './A';
 import Img from './Img';
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
 import Logo from '../../images/logo.png';
-import messages from './messages';
 import HeaderWrap from './HeaderWrap';
 import Searchbar from './Searchbar';
+import AvatarDefault from '../../images/avatar.jpg';
+import HomeIcon from '../../images/icons/home.svg';
+import ExploreIcon from '../../images/icons/explore.svg';
+import HeartIcon from '../../images/icons/heart.svg';
 
 function Header() {
   return (
@@ -19,11 +21,15 @@ function Header() {
       <Searchbar />
       <NavBar>
         <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
+          <Img className="icon" src={HomeIcon} alt="Home" />
         </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
+        <HeaderLink to="/explore">
+          <Img className="icon" src={ExploreIcon} alt="Explore" />
         </HeaderLink>
+        <HeaderLink to="/activities">
+          <Img className="icon" src={HeartIcon} alt="Heart" />
+        </HeaderLink>
+        {/* <Img className="icon-avatar" src={AvatarDefault} alt="Avatar default" /> */}
       </NavBar>
     </HeaderWrap>
   );
